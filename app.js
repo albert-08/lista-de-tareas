a75a076eda1c55d762b550ab884440f8ac543d27
Alberto Flores <alberto.floreso94@gmail.com>
2025-11-19 20:29:25 -0600

    fix js

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ function agregarTarea() {
     // Iconos
     let completar = document.createElement('i');
     completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
+    completar.addEventListener('click', completarTarea);
 
     let eliminar = document.createElement('i');
     eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+    eliminar.addEventListener('click', eliminarTarea);
 
     iconos.append(completar, eliminar);
 
@@ -39,4 +41,15 @@ function completarTarea(e) {
   tarea.classList.toggle('completada');
 }
 
+function eliminarTarea(e) {
+  let tarea = e.target.parentNode.parentNode;
+  tarea.remove();
+}
+
 boton.addEventListener('click', agregarTarea);
+
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    agregarTarea();
+  }
+});
